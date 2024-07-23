c7ba95835c94fb740a7b1355360e2f9b6025f528
codergolu21 <gouri.pusapati@gmail.com>
2024-07-23 20:39:42 +0530

    added comments

--- src/test/java/cucumber/TestNGTestRunner.java
+++ src/test/java/cucumber/TestNGTestRunner.java
@@ -6,6 +6,6 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(features = "src/test/java/cucumber",
         glue = "stepDefinitions",  monochrome = true, tags = "@ErrorValidation", plugin = {"html:target/cucumber.html"})
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
-
+// testngrunnerjava
 
 }
